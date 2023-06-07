ae9e66dd6f671bab0f660c55649afd6e1151cc75
webashraf <aliashraftamim@gmail.com>
2023-06-07 12:00:28 +0600

    add bsic login form

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,35 @@
+
+const Login = () => {
+    return (
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="card flex-shrink-0 w-full max-w-xl shadow-2xl bg-base-100">
+                        <form className="card-body w-[555px]">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+
+
+
+
+    const menu = <>
+        <NavLink to={'/'} className={({ isActive }) => isActive ? 'text-red-600 -tracking-tight' : 'text-black'}>Home</NavLink>
+
+        <NavLink to={'/instructors'} className={({ isActive }) => isActive ? 'text-red-600 ' : 'text-black'}>Instructors</NavLink>
+
+        <NavLink to={'/classes'} className={({ isActive }) => isActive ? 'text-red-600' : 'text-black'}>Classes</NavLink>
+
+        <NavLink to={'/dashboard'} className={({ isActive }) => isActive ? 'text-red-600' : 'text-black'}>Dashboard</NavLink>
+
+    </>
+
+
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
@@ -8,37 +25,19 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 gap-6 uppercase font-semibold">
+                        {menu}
                     </ul>
                 </div>
                 <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                <ul className="menu menu-horizontal px-1 gap-6 uppercase font-semibold">
+                    {menu}
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <Link to={'/login'} className="btn">Login</Link>
             </div>
         </div>
     );
