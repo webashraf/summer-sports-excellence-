9920666a5c1b895000176c5a85d26dc43d1bb092
webashraf <aliashraftamim@gmail.com>
2023-06-07 12:18:21 +0600

    add register page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,34 +1,53 @@
+import { useForm } from "react-hook-form";
+import { FaArrowRight } from 'react-icons/fa';
+import { Link } from "react-router-dom";
 
 const Login = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="card flex-shrink-0 w-full max-w-xl shadow-2xl bg-base-100">
-                        <form className="card-body w-[555px]">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body w-[555px]">
+
+
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input type="email"  {...register("email", { required: true })} placeholder="email" className="input input-bordered" />
                             </div>
+
+
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <input type="password" {...register("password", { required: true })} placeholder="password" className="input input-bordered" />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
+
+
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <button type="submit" className="btn btn-primary">Login</button>
                             </div>
                         </form>
-                    </div>
+                        <div className="w-full flex justify-center gap-4 pb-10">
+                            <button className="btn btn-outline font-bold text-2xl">Google</button>
+                            <Link to={'/register'} className="btn btn-outline font-bold text-2xl">Register                             <FaArrowRight></FaArrowRight>
+ </Link>
+                        
+        
                 </div>
             </div>
         </div>
+            </div >
+        </div >
     );
 };
 
---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,47 @@
+import { useForm } from "react-hook-form";
+
+const Register = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    return (
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="card flex-shrink-0 w-full max-w-xl shadow-2xl bg-base-100">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body w-[555px]">
+
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email"  {...register("email", { required: true })} placeholder="email" className="input input-bordered" />
+                            </div>
+
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" {...register("password", { required: true })} placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+
+
+                            <div className="form-control mt-6">
+                                <button type="submit" className="btn btn-primary">Register</button>
+                            </div>
+                        </form>
+                        <div className="w-full flex justify-center pb-10">
+                            <button className="btn btn-outline rounded-full font-bold text-2xl">G</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
