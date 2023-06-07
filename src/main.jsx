f4c747b12a9aca0cf36a76755d358b90a3562c71
webashraf <aliashraftamim@gmail.com>
2023-06-07 11:23:14 +0600

    add basic navbar and footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
+import router from './Routes/Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='max-w-[1280px] w-full mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
   </React.StrictMode>,
 )
