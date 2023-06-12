58c6970298c5ca4ba48745188266c2c8b86cf136
webashraf <aliashraftamim@gmail.com>
2023-06-12 23:29:47 +0600

    add some design in instructors page

--- src/Pages/InstructorsPage/InstructorsPage.jsx
+++ src/Pages/InstructorsPage/InstructorsPage.jsx
@@ -15,15 +15,15 @@ const InstructorsPage = () => {
             <div className="grid grid-cols-2 gap-6 py-20 px-10">
                 {
                     instructors?.map(instructor => <div key={instructor?._id} className="card card-side glass shadow-2xl">
-                        <figure><img className="h-[400px]" src={instructor?.photoUrl} alt="Movie" /></figure>
+                        <figure><img className="h-[400px] w-[250px]" src={instructor?.photoUrl} alt="Movie" /></figure>
                         <div className="card-body">
 
                             <div className="space-y-3">
-                                <h2 className="card-title text-2xl"><span className="font-bold">Name : </span>{instructor?.name}</h2>
-                                <p className="text-lg"><span className="font-bold">Email : </span>{instructor?.email}</p>
-                                <p className="text-lg"><span className="font-bold">Total Class : </span>{instructor?.totalClass ? instructor?.totalClass : 0}</p>
+                                <h2 className="card-title text-2xl uppercase font-serif"><span className="font-bold ">Name : </span>{instructor?.name}</h2>
+                                <p className="text-md "><span className="font-bold">Email : </span><span className="underline">{instructor?.email}</span></p>
+                                <p className="text-md"><span className="font-bold">Total Class : </span>{instructor?.totalClass ? instructor?.totalClass : 0}</p>
                                 <ul>
-                                    <span className="font-bold text-lg">Class Name : </span>
+                                    <span className="font-bold text-md">Class Name : </span>
                                     {
                                         instructor?.classes ? instructor?.classes.map((className, i) => <li className="ml-6 text-md list-decimal" key={i}>{className}</li>) : <h4>1. Class name is not found</h4>
                                     }
